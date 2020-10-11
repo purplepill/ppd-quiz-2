1877a59cc6993c71630c633f6dedc8447d40f019
Erica <erica.gucciardo@updater.com>
2020-10-11 18:33:12 -0400

    MVP 1 - basic functionality

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,175 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import questions from '../questions.json';
+import { useEffect, useState } from 'react';
+
+const WEIGHTS = {
+  MORALITY: 1.4214,
+  BOTH: 1.08027,
+  REALITY: 0.73913,
+};
+
+const ANSWERS = {
+  stronglyAgree: 1,
+  agree: 0.75,
+  neutral: 0.5,
+  disagree: 0.25,
+  stronglyDisagree: 0,
+};
+
+const toScore = (obj) => Object.values(obj).reduce((sum, val) => sum + val, 0);
+
+const toOverallScore = (rpScore, bpScore) => {
+  const rp = toScore(rpScore);
+  const bp = toScore(bpScore);
+  const total = rp - bp + 20; // (-20 -> 30 => shift over by 20)
+  const percent = total * 2;
+  return percent.toFixed(0);
+};
+
+const flattened = Object.entries(questions).reduce((array, [key, qs]) => {
+  return [
+    ...array,
+    ...qs.map((question) => ({
+      question,
+      key,
+    })),
+  ];
+}, []);
 
 export default function Home() {
+  const [rpScore, updateRpScore] = useState({});
+  const [bpScore, updateBpScore] = useState({});
+
+  useEffect(() => {
+    try {
+      const scores = JSON.parse(localStorage.getItem('score'));
+      updateRpScore(scores.rpScore);
+      updateBpScore(scores.bpScore);
+    } catch {}
+  }, []);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      localStorage.setItem(
+        'score',
+        JSON.stringify({
+          rpScore,
+          bpScore,
+        })
+      );
+    }
+  }, [rpScore, bpScore]);
+
+  const answer = (ind, questionType, answeredValue) => () => {
+    const [pillType, moralityOrReality] = questionType.split('_');
+    const score = WEIGHTS[moralityOrReality] * answeredValue;
+    if (pillType === 'RP') {
+      updateRpScore({
+        ...rpScore,
+        [ind]: score,
+      });
+    }
+    if (pillType === 'BP') {
+      updateBpScore({
+        ...bpScore,
+        [ind]: score,
+      });
+    }
+  };
+
+  const checked = (ind, questionType, answeredValue) => {
+    let map;
+    const [pillType, moralityOrReality] = questionType.split('_');
+    if (pillType === 'RP') {
+      map = rpScore;
+    }
+    if (pillType === 'BP') {
+      map = bpScore;
+    }
+    if (!map.hasOwnProperty(ind)) {
+      return false;
+    }
+    const score = WEIGHTS[moralityOrReality] * answeredValue;
+    return map[ind] === score;
+  };
+
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div>
+        {flattened.map(({ key, question }, ind) => (
+          <div>
+            {/* <>{key}</> */}
+            <>
+              <div>
+                <h4>
+                  {ind + 1}: {question}
+                </h4>
+                <input
+                  name={question}
+                  id={`${ind}_stronglyAgree`}
+                  type='radio'
+                  value='stronglyAgree'
+                  onChange={answer(ind, key, ANSWERS.stronglyAgree)}
+                  checked={checked(ind, key, ANSWERS.stronglyAgree)}
+                />
+                <label htmlFor={`${ind}_stronglyAgree`}>Strongly Agree</label>
+                <input
+                  name={question}
+                  id={`${ind}_agree`}
+                  type='radio'
+                  value='agree'
+                  onChange={answer(ind, key, ANSWERS.agree)}
+                  checked={checked(ind, key, ANSWERS.agree)}
+                />
+                <label htmlFor={`${ind}_agree`}>Agree</label>
+                <input
+                  name={question}
+                  id={`${ind}_neutral`}
+                  type='radio'
+                  value='neutral'
+                  onChange={answer(ind, key, ANSWERS.neutral)}
+                  checked={checked(ind, key, ANSWERS.neutral)}
+                />
+                <label htmlFor={`${ind}_neutral`}>Neutral</label>
+                <input
+                  name={question}
+                  id={`${ind}_disagree`}
+                  type='radio'
+                  value='disagree'
+                  onChange={answer(ind, key, ANSWERS.disagree)}
+                  checked={checked(ind, key, ANSWERS.disagree)}
+                />
+                <label htmlFor={`${ind}_disagree`}>Disagree</label>
+                <input
+                  name={question}
+                  id={`${ind}_stronglyDisagree`}
+                  type='radio'
+                  value='stronglyDisagree'
+                  onChange={answer(ind, key, ANSWERS.stronglyDisagree)}
+                  checked={checked(ind, key, ANSWERS.stronglyDisagree)}
+                />
+                <label htmlFor={`${ind}_stronglyDisagree`}>
+                  Strongly Disagree
+                </label>
+              </div>
+            </>
+          </div>
+        ))}
+      </div>
+      <div>
+        <div>RP Score: {toScore(rpScore).toFixed(2)}</div>
+        <div>BP Score: {toScore(bpScore).toFixed(2)}</div>
+        <div>Overall RPness {toOverallScore(rpScore, bpScore)}%</div>
+        <button
+          onClick={() => {
+            updateBpScore({});
+            updateRpScore({});
+          }}
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+          CLEAR ANSWERS
+        </button>
+      </div>
     </div>
-  )
+  );
 }
